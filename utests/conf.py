8078d6f8aca87231c3302428e034293836b13d42
Zephor <zephor@qq.com>
2016-01-06 11:23:15 +0800

    remove bad test and add abspath

---
+++ utests/conf.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+import os
+
+__author__ = 'zephor'
+
+
+TEST_ROOT = os.path.abspath(os.path.dirname(__file__))
--- utests/test_parsers.py
+++ utests/test_parsers.py
@@ -1,19 +1,9 @@
 # coding=utf-8
 import unittest
 
-from zspider.parsers import get_parser
-
 __author__ = 'zephor'
 
 
+# noinspection PyUnresolvedReferences
 class TestNewspaper(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        from www.handlers import app
-        import utils
-        utils.engine.init_app(app)
-        parser = get_parser('5624a883bada213cd9c4e3ac', 'test', 'Newspaper')
-
-    def test_init(self):
-        pass
+    pass
